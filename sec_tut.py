9c70ee1c4eaa5c1cbeba55a935dc98d27788f753
barsatrai <barsatrai777@gmail.com>
2024-06-24 09:20:26 +0545

    second tutorial

---
+++ sec_tut.py
@@ -0,0 +1,12 @@
+# Arithmatic operations--
+print(3*3) # multiplication
+print(3+3) # addition
+print(3-3) # subtraction
+print(2**3) #exponent
+print(5/2) 
+print(5//2)
+print(5%2)
+x = 4
+y = 5
+x += y # =x+y
+print(x)
